433f258d9d7ff4bfb431e6bb4c78a5b022d13b96
Heet <heetnandu01@gmail.com>
2025-06-28 23:59:26 +0530

    Project completed

---
+++ TextAnalyzer.java
@@ -0,0 +1,93 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class TextAnalyzer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the paragraph: ");
+        String paragraph = sc.nextLine();
+
+        countWords(paragraph);
+        findMostFrequentWord(paragraph);
+        getWordFrequencyMap(paragraph);
+        findFirstUniqueWord(paragraph);
+
+    }
+
+    static void countWords(String text) {
+        text = text.trim();
+        if (text.isEmpty()) {
+            System.out.println("No words found");
+            return;
+        }
+        String[] words = text.split("\\s+");
+        System.out.println("word count: " + words.length);
+    }
+
+    static void findMostFrequentWord(String text) {
+        text = text.toLowerCase();
+        text = text.replaceAll("[^a-zA-Z\\s]", "");
+        String words[] = text.split("\\s+");
+
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            if (wordCount.containsKey(word)) {
+                wordCount.put(word, wordCount.get(word) + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
+        }
+        String mostFrequentWord = "";
+        int maxCount = 0;
+
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                mostFrequentWord = entry.getKey();
+            }
+        }
+        System.out.println("Most frequent word: " + mostFrequentWord + " (" + maxCount + " times)");
+
+    }
+
+    static void getWordFrequencyMap(String text) {
+        text = text.toLowerCase();
+        text = text.replaceAll("[^a-zA-Z\\s]", "");
+
+        String words[] = text.split("\\s+");
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            if (wordCount.containsKey(word)) {
+                wordCount.put(word, wordCount.get(word) + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
+        }
+    }
+
+    static void findFirstUniqueWord(String text) {
+        text = text.toLowerCase();
+        text = text.replaceAll("[^a-zA-Z\\s]", "");
+        String words[] = text.split("\\s+");
+
+        Map<String, Integer> wordCount = new LinkedHashMap<>();
+
+        for (String word : words) {
+            if (wordCount.containsKey(word)) {
+                wordCount.put(word, wordCount.get(word) + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println("First unique word: " + entry.getKey());
+                break;
+            }
+        }
+
+    }
+}
